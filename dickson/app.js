d364b7a7357b2130f020d025aa3c2d9c845293ce
unknown <dicksonn82@gmail.com>
2023-03-03 12:18:58 +0300

    new changes

--- dickson/app.js
+++ dickson/app.js
@@ -7,9 +7,9 @@ function pageTransitions(){
     //button click active class
 
     for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click',function() {
-            let currentBtn= document.querySelectorAll('.active-btn');
-            currentBtn[0].className=currentBtn[0].className.replace('active-btn','');
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn= document.querySelectorAll ('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace ('active-btn','');
             this.className +='active-btn';
         })
     }
