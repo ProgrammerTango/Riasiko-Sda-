b025d64d152412cebec73727e520bb19a88ed5f6
unknown <dicksonn82@gmail.com>
2023-03-14 10:32:40 +0300

    first commit

---
+++ port3/script.js
@@ -0,0 +1,7 @@
+/*==========================typing animation======================*/
+var typed = new Typed(".typing", {
+    strings:["","Web designer","Web developer", " and Graphic designer"],
+    typeSpeed:80,
+    backspeed:60,
+    loop:true 
+})
